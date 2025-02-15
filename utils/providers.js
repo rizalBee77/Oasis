cd7505d6f730d1408aa8b18016477c377f05275e
0xRizalxyz <72789671+rizalBee77@users.noreply.github.com>
2025-02-16 01:30:27 +0700

    Add files via upload

---
+++ utils/providers.js
@@ -0,0 +1,106 @@
+import { generateRandomId } from "./system.js";
+import { readToken, saveToken } from "./file.js";
+import { logger } from "./logger.js";
+import axios from 'axios';
+import fs from 'fs';
+
+async function connectWithToken(token) {
+    const url = 'https://api.distribute.ai/internal/auth/connect';
+    const randomId = generateRandomId();
+    const payload = {
+        "name": randomId,
+        "platform": "browser"
+    }
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': token,
+    };
+
+    try {
+        const response = await axios.post(url, payload, { headers });
+        const logToken = response.data.token;
+        logger('Creating Providers successful:', logToken);
+        return logToken;
+    } catch (error) {
+        logger('Creating Providers error:', error.response ? error.response.status : error.response.statusText, 'error');
+        return null;
+    }
+}
+
+async function getAllProviders(token) {
+    const url = 'https://api.distribute.ai/internal/provider/providers?limit=100';
+
+    const headers = {
+        'Authorization': token,
+    };
+
+    try {
+        const response = await axios.get(url, { headers });
+        const data = response.data;
+        logger('Getting All Existing Providers successful');
+        return data;
+    } catch (error) {
+        logger('Getting All Existing Providers error:', error.response ? error.response.status : error.response.statusText, 'error');
+        return null;
+    }
+}
+
+async function deleteProviders(token, nodeId) {
+    const url = `https://api.distribute.ai/internal/provider/?id=${nodeId}`;
+    const headers = {
+        'Authorization': token,
+    };
+
+    try {
+        const response = await axios.delete(url, { headers });
+        const data = response.data;
+        logger('Deleting Providers successful');
+        return data;
+    } catch (error) {
+        logger('Deleting Providers error:', error.response ? error.response.status : error.response.statusText, 'error');
+        return null;
+    }
+}
+
+export async function createProviders(numID) {
+    try {
+        const tokens = await readToken('tokens.txt');
+        const filePath = 'providers.txt';
+
+        if (fs.existsSync(filePath)) {
+            try {
+                fs.unlinkSync(filePath);
+                logger(`${filePath} has been deleted.`);
+            } catch (err) {
+                logger(`Error deleting ${filePath}:`, 'error');
+            }
+        }
+
+        for (const token of tokens) {
+            logger(`Checking All Providers using token: ${token}`);
+            const response = await getAllProviders(token)
+            const nodeIds = response.results.map(item => item.id);
+            logger(`Found ${nodeIds.length} existing providers - trying to delete old providers...`);
+            for (const nodeId of nodeIds) {
+                await deleteProviders(token, nodeId)
+            }
+
+            logger(`Creating Providers using token: ${token}`);
+            for (let i = 0; i < numID; i++) {
+                logger(`Creating Providers #${i + 1}....`);
+                const logToken = await connectWithToken(token);
+                if (logToken) {
+                    saveToken("providers.txt", logToken)
+                } else {
+                    logger('Failed to create provider', 'error', 'error');
+                    continue;
+                }
+            };
+
+        };
+        return true;
+    } catch (error) {
+        logger("Error reading token or connecting:", error, 'error');
+    };
+};
---
+++ utils/websocket.js
@@ -0,0 +1,99 @@
+import WebSocket from "ws";
+import { HttpsProxyAgent } from "https-proxy-agent";
+import { generateRandomId, generateRandomSystemData } from "./system.js";
+import { delay } from "./file.js";
+import { logger } from "./logger.js";
+
+export async function createConnection(token, proxy = null) {
+    const wsOptions = {
+        headers: {
+            'accept-encoding': 'gzip, deflate, br, zstd',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'no-cache',
+            connection: 'Upgrade',
+            host: 'ws.distribute.ai',
+            origin: 'chrome-extension://knhbjeinoabfecakfppapfgdhcpnekmm',
+            pragma: 'no-cache',
+            'sec-websocket-extensions': 'permessage-deflate; client_max_window_bits',
+            'sec-websocket-version': '13',
+            upgrade: 'websocket',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
+        }
+    };
+
+    if (proxy) {
+        logger(`Connect Using proxy: ${proxy}`);
+        wsOptions.agent = new HttpsProxyAgent(proxy);
+    }
+
+    const socket = new WebSocket(`wss://ws.distribute.ai/?token=${token}&version=0.1.22&platform=extension`, wsOptions);
+
+    socket.on("open", async () => {
+        logger(`WebSocket connection established for providers: ${token}`, "", "success");
+        const randomId = generateRandomId();
+        const systemData = generateRandomSystemData();
+
+        socket.send(JSON.stringify(systemData));
+        //await delay(1000);
+
+        socket.send(
+            JSON.stringify({
+                id: randomId,
+                type: "heartbeat",
+                data: {
+                    inferenceState: true,
+                    version: "0.1.22",
+                    mostRecentModel: "unknown",
+                    status: "active",
+                },
+            })
+        );
+
+        setInterval(() => {
+            const randomId = generateRandomId();
+            socket.send(
+                JSON.stringify({
+                    id: randomId,
+                    type: "heartbeat",
+                    data: {
+                        inferenceState: true,
+                        version: "0.1.22",
+                        mostRecentModel: "unknown",
+                        status: "active",
+                    },
+                })
+            );
+        }, 60000);
+    });
+
+    socket.on("message", (data) => {
+        const message = data.toString();
+        try {
+            const parsedMessage = JSON.parse(message);
+            if (parsedMessage.type === "serverMetrics") {
+                const { totalEarnings, totalUptime, creditsEarned } = parsedMessage.data;
+                logger(`Heartbeat sent for provider: ${token}`);
+                logger(`Total uptime: ${totalUptime} seconds | Credits earned:`, creditsEarned);
+            } else if (parsedMessage.type === "acknowledged") {
+                logger("System Updated:", message, "info");
+            } else if (parsedMessage.type === "error" && parsedMessage.data.code === "Invalid body") {
+                const systemData = generateRandomSystemData();
+                socket.send(JSON.stringify(systemData));
+            }
+        } catch (error) {
+            logger("Error parsing message:", "error");
+        }
+    });
+
+    socket.on("close", () => {
+        logger("WebSocket connection closed for token:", token, "warn");
+        setTimeout(() => {
+            logger("Attempting to reconnect for token:", token, "warn");
+            createConnection(token, proxy);
+        }, 5000);
+    });
+
+    socket.on("error", (error) => {
+        logger("WebSocket error for token:", token, "error");
+    });
+}
